155bc4432b02f97122b7af4630f767c660a43ed1
alexandre <alexandrebezao@gmail.com>
2016-04-10 08:04:42 -0300

    Exercicio 2

--- src/SON/CategoriaBundle/Controller/DefaultController.php
+++ src/SON/CategoriaBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace SON\CategoriaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use stdClass;
 
 class DefaultController extends Controller
 {
@@ -13,28 +14,40 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        $categorias = $this->listarCategorias();
+        $categorias = $this->listarCategoriasObj();
         return $this->render('CategoriaBundle:Default:exercicio1.html.twig',array('categorias' => $categorias));
     }
 
     public function buscarCategoriaMaisProdutoAction($nomeCategoria, $nomeProduto)
     {
-        $nomeCategoria = strtoupper($nomeCategoria);
-        $nomeProduto = strtoupper($nomeProduto);
+        $nomeCategoria  = strtoupper($nomeCategoria);
+        $nomeProduto    = strtoupper($nomeProduto);
 
-        $categoriaArray = $this->listarCategorias();
+        $categorias = $this->listarCategoriasObj();
 
-        $mensagem = null;
-        if (! array_key_exists($nomeCategoria, $categoriaArray)) {
-            $mensagem = "Categoria nao cadastrada";
-            return $this->render('CategoriaBundle:Default:exercicio2.html.twig',array('mensagem' => $mensagem));
+        $achouCategoria = false;
+        $achouProduto   = false;
+        foreach($categorias as $c){
+            if($c->nomeCategoria == $nomeCategoria){
+                $achouCategoria = true;
+                foreach($c->produtos as $p){
+                    if($p->nomeProduto == $nomeProduto){
+                        $achouProduto = true;
+                        break;
+                    }
+                }
+                break;
+            }
         }
 
-        $categoria = $categoriaArray[$nomeCategoria];
-        if (! in_array($nomeProduto, $categoria)) {
-            $mensagem = "O produto $nomeProduto nao esta cadastrado ou nao pertence a categoria $nomeCategoria";
+        if($achouCategoria){
+            if($achouProduto){
+                $mensagem = "A categoria $nomeCategoria é do produto $nomeProduto.";
+            }else{
+                $mensagem = "O produto $nomeProduto nao esta cadastrado ou nao pertence a categoria $nomeCategoria";
+            }
         }else{
-            $mensagem = "A categoria $nomeCategoria é do produto $nomeProduto.";
+            $mensagem = "Categoria nao cadastrada";
         }
 
         return $this->render('CategoriaBundle:Default:exercicio2.html.twig',array('mensagem' => $mensagem));
@@ -50,4 +63,80 @@ class DefaultController extends Controller
 
         return $categorias;
     }
+
+    private function listarCategoriasObj()
+    {
+        $categorias = array();
+
+        $cat1 = new stdClass();
+        $cat1->idCategoria = 1;
+        $cat1->nomeCategoria = "COMIDA";
+        $cat1->produtos = $this->listarProdutosObj($cat1->idCategoria);
+        $categorias[] = $cat1;
+
+        $cat2 = new stdClass();
+        $cat2->idCategoria = 2;
+        $cat2->nomeCategoria = "HIGIENE";
+        $cat2->produtos = $this->listarProdutosObj($cat2->idCategoria);
+        $categorias[] = $cat2;
+
+        $cat3 = new stdClass();
+        $cat3->idCategoria = 3;
+        $cat3->nomeCategoria = "LIMPEZA";
+        $cat3->produtos = $this->listarProdutosObj($cat3->idCategoria);
+        $categorias[] = $cat3;
+
+        return $categorias;
+    }
+
+    private function listarProdutosObj($idCategoria)
+    {
+        $produtos = array();
+        switch ($idCategoria){
+            case 1:
+                $prod1 = new stdClass();
+                $prod1->idProduto = 1;
+                $prod1->nomeProduto = "ARROZ";
+                $prod1->idCategoria = 1;
+                $produtos[] = $prod1;
+
+                $prod2 = new stdClass();
+                $prod2->idProduto = 2;
+                $prod2->nomeProduto = "FEIJAO";
+                $prod2->idCategoria = 1;
+                $produtos[] = $prod2;
+            break;
+
+            case 2:
+                $prod1 = new stdClass();
+                $prod1->idProduto = 3;
+                $prod1->nomeProduto = "SHAMPO";
+                $prod1->idCategoria = 2;
+                $produtos[] = $prod1;
+
+                $prod2 = new stdClass();
+                $prod2->idProduto = 4;
+                $prod2->nomeProduto = "SABONETE";
+                $prod2->idCategoria = 2;
+                $produtos[] = $prod2;
+            break;
+
+            case 3:
+                $prod1 = new stdClass();
+                $prod1->idProduto = 5;
+                $prod1->nomeProduto = "DETERGENTE";
+                $prod1->idCategoria = 3;
+                $produtos[] = $prod1;
+
+                $prod2 = new stdClass();
+                $prod2->idProduto = 6;
+                $prod2->nomeProduto = "ESPONJA";
+                $prod2->idCategoria = 3;
+                $produtos[] = $prod2;
+            break;
+        }
+
+        return $produtos;
+    }
+
 }
