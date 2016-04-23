ef177061d9d01b59cc2bfcccfd0909a6bc30f3e3
alexandre <alexandrebezao@gmail.com>
2016-04-23 00:07:03 -0300

    Modulo 5 - FIXTURES Aula 1 | Carregando os Dados

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new SON\CatalogoBundle\CatalogoBundle(),
             new SON\CategoriaBundle\CategoriaBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
